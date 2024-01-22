c5f011bd2932d09616e26533779a8269ff8e147c
Prudaev Ilia <104941493+prudaev@users.noreply.github.com>
2024-01-22 13:49:01 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+    prompt("Напишите свой номер телефона для связи");
+    alert("Наш специалист свяжется с Вами в ближайшее время, спасибо за обращение");
+    
+});
+
+let btns = document.querySelector(".btn-order");
+
+btns.addEventListener("click", function(){
+    alert("Как только будет база обязательно рассчитаем стоимоть кухни мечты")
+});
